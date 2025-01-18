fix data leak, add time_steps

qlstm/models/LSTM.py
@@ -4,11 +4,10 @@ import torch.nn as nn
 
 class LSTMCell(nn.Module):
     def __init__(self, input_size, hidden_size):
-        super(LSTMCell, self).__init__()
+        super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        # Weight matrices for input gate, forget gate, cell gate, and output gate
         self.W_ih = nn.Parameter(torch.Tensor(4 * hidden_size, input_size))
         self.W_hh = nn.Parameter(torch.Tensor(4 * hidden_size, hidden_size))
         self.b_ih = nn.Parameter(torch.Tensor(4 * hidden_size))
@@ -17,7 +16,6 @@ class LSTMCell(nn.Module):
         self._init_weights()
 
     def _init_weights(self):
-        # Initialize weights
         nn.init.xavier_uniform_(self.W_ih)
         nn.init.xavier_uniform_(self.W_hh)
         nn.init.zeros_(self.b_ih)
@@ -26,7 +24,6 @@ class LSTMCell(nn.Module):
     def forward(self, x: torch.Tensor, hidden):
         h_prev, c_prev = hidden
 
-        # Linear transformations
         gates = (
             torch.matmul(x, self.W_ih.t())
             + torch.matmul(h_prev, self.W_hh.t())
@@ -34,16 +31,13 @@ class LSTMCell(nn.Module):
             + self.b_hh
         )
 
-        # Split gates
         input_gate, forget_gate, cell_gate, output_gate = gates.chunk(4, dim=1)
 
-        # Apply activation functions
         input_gate = torch.sigmoid(input_gate)
         forget_gate = torch.sigmoid(forget_gate)
         cell_gate = torch.tanh(cell_gate)
         output_gate = torch.sigmoid(output_gate)
 
-        # Update cell state and hidden state
         c_next = forget_gate * c_prev + input_gate * cell_gate
         h_next = output_gate * torch.tanh(c_next)
 
@@ -52,7 +46,7 @@ class LSTMCell(nn.Module):
 
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers=1):
-        super(LSTM, self).__init__()
+        super().__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.cells = nn.ModuleList(
@@ -67,11 +61,11 @@ class LSTM(nn.Module):
 
         if hidden is None:
             h = [
-                torch.zeros(batch_size, self.hidden_size).to(x.device)
+                torch.zeros(batch_size, self.hidden_size, device=x.device)
                 for _ in range(self.num_layers)
             ]
             c = [
-                torch.zeros(batch_size, self.hidden_size).to(x.device)
+                torch.zeros(batch_size, self.hidden_size, device=x.device)
                 for _ in range(self.num_layers)
             ]
         else:

qlstm/modules/data.py
@@ -1,35 +1,34 @@
 from pathlib import Path
 from typing import Optional, Sequence
 
+import numpy as np
 import pandas as pd
 import torch
 from lightning.pytorch import LightningDataModule
 from rich import print
 from rich.table import Table
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader, Dataset, Subset, random_split
+from torch.utils.data import ConcatDataset, DataLoader, Dataset
 
 from modules.utils import workers_handler
 
 
-class DataModule(Dataset):
+class CustomDataset(Dataset):
     """Pytorch Data Module"""
 
-    def __init__(self, corpus, labels):
-        self.corpus = corpus
-        self.labels = labels
+    def __init__(self, inputs, labels):
+        self.inputs = torch.tensor(inputs, dtype=torch.float32)
+        self.labels = torch.tensor(labels, dtype=torch.float32)
+
+        if len(self.inputs) != len(self.labels):
+            raise IndexError("The length of the inputs and labels do not match.")
 
     def __len__(self):
-        return len(self.corpus)
+        return len(self.inputs)
 
     def __getitem__(self, idx):
-        text = self.corpus[idx]
-        label = self.labels[idx]
-
-        text = torch.tensor(text, dtype=torch.float32)
-        label = torch.tensor(label, dtype=torch.float32)
-
-        return text, label
+        return self.inputs[idx], self.labels[idx]
 
 
 class CustomDataModule(LightningDataModule):
@@ -37,6 +36,7 @@ class CustomDataModule(LightningDataModule):
         self,
         data_path: str,
         batch_size: int = 32,
+        time_steps: int = 1,
         data_limit: Optional[float] = None,
         split_size: Sequence[float] = (0.75, 0.15, 0.1),
         num_workers: int = 0,
@@ -48,13 +48,17 @@ class CustomDataModule(LightningDataModule):
         Args:
             data_path (str): Path to the dataset.
             batch_size (int, optional): Batch size for data loading. Default: 32
-            data_limit (float, optional): Limit for the size of the dataset (0 -> 1.0). Default: None
-            split_size (sequence, optional): Proportions for train, validation, and test splits. Default: (0.75, 0.15, 0.1)
-            num_workers (int, optional): Number of data loading workers. Default: 0
-            pin_memory (bool, optional): Whether to pin memory for faster data transfer. Default: True
+            time_steps (int, optional): Number of time steps to include in each input sequence. Default: 1
+            data_limit (float, optional): Limit for the size of the dataset as a fraction (0 -> 1.0).
+                                          If None, use the full dataset. Default: None
+            split_size (Sequence[float], optional): Proportions for train, validation, and test splits.
+                                                    The values should sum to 1.0. Default: (0.75, 0.15, 0.1)
+            num_workers (int, optional): Number of workers for data loading in parallel. Default: 0
+            pin_memory (bool, optional): Whether to pin memory for faster data transfer to GPU. Default: True
         """
         super().__init__()
         self.data_path = Path(data_path)
+        self.time_steps = time_steps
         self.data_limit = self._check_limit(data_limit)
         self.split_size = split_size
         self.loader_config = {
@@ -62,16 +66,17 @@ class CustomDataModule(LightningDataModule):
             "num_workers": workers_handler(num_workers),
             "pin_memory": pin_memory,
         }
+        self.encoder = {}
 
     @staticmethod
     def _check_limit(value: Optional[float]) -> Optional[float]:
+        "Check input value for limit."
         if isinstance(value, float) and 0 < value < 1:
             return value
 
-    def _limit_data(self, data: Dataset) -> Subset:
-        return random_split(
-            dataset=data, lengths=(self.data_limit, 1 - self.data_limit)
-        )[0]
+    def _limit_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        "Apply limit to the data."
+        return data[: int(len(data) * self.data_limit)]
 
     def _summary(self) -> None:
         table = Table(title="[bold]Sets Distribution[/]")
@@ -116,43 +121,68 @@ class CustomDataModule(LightningDataModule):
         # Clean up
         self.dataframe = data.dropna().drop_duplicates()
 
-        # Process data
-        data = self.dataframe[
-            [
-                "Measured Power",
-                "NWP Radiation",
-                "NWP Rainfall",
-                "NWP Temperature",
-                "NWP Pressure",
-                "NWP Windspeed",
-                "Measured Radiation",
-            ]
-        ]
-
-        self.encoder = {"input": MinMaxScaler(), "output": MinMaxScaler()}
-
-        inputs = data[:-1:].values
-        inputs = self.encoder["input"].fit_transform(inputs)
-
-        labels = data[["Measured Power"]][1::].values
-        labels = self.encoder["output"].fit_transform(labels)
-
-        # Modulize data
-        data = DataModule(inputs, labels)
-
         # Limit data
         if self.data_limit:
             data = self._limit_data(data)
 
-        # Finalize
-        self.dataset = data
+        # Select data
+        features = [
+            "Measured Power",
+            "NWP Radiation",
+            "NWP Rainfall",
+            "NWP Temperature",
+            "NWP Pressure",
+            "NWP Windspeed",
+            "Measured Radiation",
+        ]
 
-    def setup(self, stage: str):
-        if not hasattr(self, "train_set"):
-            self.train_set, self.val_set, self.test_set = random_split(
-                dataset=self.dataset, lengths=self.split_size
+        data = self.dataframe[features]
+
+        # Encode data
+        for feature in features:
+            self.encoder[feature] = MinMaxScaler()
+            data.loc[:, feature] = (
+                self.encoder[feature]
+                .fit_transform(data[[feature]].values)
+                .astype("object")
             )
 
+        # Create inputs and labels
+        inputs = data.values
+        labels = data[["Measured Power"]].values
+
+        # Create time steps
+        inputs = [
+            inputs[i : i + self.time_steps]
+            for i in range(len(inputs) - self.time_steps)
+        ]
+        inputs = np.array(inputs)
+
+        labels = [
+            labels[i + self.time_steps] for i in range(len(labels) - self.time_steps)
+        ]
+        labels = np.array(labels)
+
+        # Train, val, test split
+        X_train, X_temp, y_train, y_temp = train_test_split(
+            inputs, labels, train_size=self.split_size[0], shuffle=False
+        )
+
+        val_size = self.split_size[1] / (1 - self.split_size[0])
+
+        X_val, X_test, y_val, y_test = train_test_split(
+            X_temp, y_temp, train_size=val_size, shuffle=False
+        )
+
+        # Modulize data
+        self.train_set = CustomDataset(X_train, y_train)
+        self.val_set = CustomDataset(X_val, y_val)
+        self.test_set = CustomDataset(X_test, y_test)
+
+        # Finalize
+        self.dataset = ConcatDataset([self.train_set, self.val_set, self.test_set])
+
+    def setup(self, stage: str):
         if stage == "fit":
             self._summary()
 

qlstm/test.py
@@ -5,7 +5,6 @@ import rootutils
 import torch
 from omegaconf import DictConfig
 from rich import print, traceback
-from rich.prompt import Prompt
 
 rootutils.autosetup()
 traceback.install()
@@ -21,29 +20,36 @@ def main(cfg: DictConfig) -> None:
     shutil.rmtree("outputs")
 
     # Define dataset
-    dataset = CustomDataModule(data_path=cfg["data_path"])
+    dataset = CustomDataModule(data_path=cfg["data_path"], time_steps=cfg["time_steps"])
     dataset.prepare_data()
 
     # Define model
-    model = LSTM(input_size=7, hidden_size=64, num_layers=1)
+    model = LSTM(input_size=7, hidden_size=128, num_layers=4)
 
     # Define lightning model
     lit_model = LitModel(model=model, checkpoint=cfg["checkpoint"]).eval()
 
+    torch.set_printoptions(4, sci_mode=False)
     # Inference loop
     with torch.inference_mode():
         while True:
-            X = torch.tensor(
-                [float(v.replace(",", "")) for v in Prompt.ask("Input").split()]
-            )
+            index = input("Index: ")
 
-            X = dataset.encoder["input"].transform(X.reshape(1, -1))
+            try:
+                index = int(index)
+            except Exception:
+                exit()
 
-            out = lit_model(torch.tensor(X, dtype=torch.float32))
+            X, y = dataset.dataset[index]
+            print(X)
 
-            out = dataset.encoder["output"].inverse_transform(out)
+            out = lit_model(X.unsqueeze(0))
 
-            print(f"Output: {out.reshape(-1)}\n")
+            y = dataset.encoder["Measured Power"].inverse_transform(y.reshape(-1, 1))
+            out = dataset.encoder["Measured Power"].inverse_transform(out)
+
+            print(f"Label: {y.item()}")
+            print(f"Output: {out.item()}\n")
 
 
 if __name__ == "__main__":

qlstm/train.py
@@ -42,7 +42,7 @@ def main(cfg: DictConfig) -> None:
     )
 
     # Define model
-    model = LSTM(input_size=7, hidden_size=64, num_layers=1)
+    model = LSTM(input_size=7, hidden_size=128, num_layers=4)
 
     # Setup loss
     loss = nn.SmoothL1Loss()