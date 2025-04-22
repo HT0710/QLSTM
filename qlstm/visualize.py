f906ba8ce681164c8ee127ecb06cc733fd664e71
ArtA <pthung7102002@gmail.com>
2025-04-23 06:40:52 +0700

    Update visualize.py

--- qlstm/visualize.py
+++ qlstm/visualize.py
@@ -13,78 +13,123 @@ rootutils.autosetup()
 traceback.install()
 
 from models.cLSTM import cLSTM
-from modules.data import CustomDataModule
+from models.cQLSTM import cQLSTM
+from models.LSTM import LSTM
+from models.QLSTM import QLSTM
+from modules.data import CDM2
 from modules.model import LitModel
 
 
 @hydra.main(config_path="./configs", config_name="test", version_base="1.3")
 def main(cfg: DictConfig) -> None:
-    seed_everything(seed=42, workers=True)
-
     # Remove the hydra outputs
     shutil.rmtree("outputs")
 
+    seed_everything(seed=42, workers=True)
+
     # Define dataset
-    dataset = CustomDataModule(
+    dataset = CDM2(
         data_path="qlstm/data/place_1_new.csv",
         features="4-10",
         labels=[4],
         time_steps=24,
         overlap=True,
-        n_future=1,
     )
     dataset.prepare_data()
     dataset.setup("predict")
 
     # Define model
-    model = cLSTM(
+    model = QLSTM(
+        input_size=10,
+        hidden_size=352,
+        n_qubits=4,
+    )
+    # Define lightning model
+    lit_model3 = LitModel(
+        model=model,
+        checkpoint="lightning_logs/QLSTM/version_3/checkpoints/last.ckpt",
+        device="cpu",
+    ).eval()
+
+    # Define model
+    model = LSTM(input_size=10, hidden_size=23)
+    # Define lightning model
+    lit_model1 = LitModel(
+        model=model,
+        checkpoint="lightning_logs/LSTM/version_1/checkpoints/last.ckpt",
+        device="cpu",
+    ).eval()
+
+    # Define model
+    model = cLSTM(input_size=10, hidden_size=23)
+    # Define lightning model
+    lit_model2 = LitModel(
+        model=model,
+        checkpoint="lightning_logs/cLSTM/version_11/checkpoints/last.ckpt",
+        device="cpu",
+    ).eval()
+
+    # Define model
+    model = cQLSTM(
         input_size=10,
         hidden_size=128,
-        # n_qubits=2,
+        n_qubits=4,
     )
-
     # Define lightning model
-    lit_model = LitModel(
+    lit_model4 = LitModel(
         model=model,
-        output_size=dataset.n_future,
-        checkpoint="lightning_logs/cLSTM/version_1/checkpoints/epoch=24-step=1500.ckpt",
+        checkpoint="lightning_logs/cQLSTM/version_0/checkpoints/last.ckpt",
+        device="cpu",
     ).eval()
+
+    plt.figure(figsize=(16, 9))
+
+    X, y = dataset.val_set[:168]
+    # X, y = X[1:], y[:-1, :1]
+
+    y = dataset.encoder["label"].inverse_transform(y)
+
+    plt.plot(range(len(y)), y, label="Actual", linewidth=3)
 
     # Inference loop
     with torch.inference_mode():
-        X, y = dataset.val_set[:168]
-        X, y = X[1:], y[:-1, :1]
+        out = np.array(lit_model1(X))
 
-        out = np.array([lit_model(x.unsqueeze(0)).squeeze(0) for x in X])
+        out = dataset.encoder["label"].inverse_transform(out)
 
-        y = dataset.encoder["label"].inverse_transform(y)
-        outs = dataset.encoder["label"].inverse_transform(out).transpose(1, 0)
+        plt.plot(range(len(out)), out, label="LSTM", linewidth=3)
 
-        plt.figure(figsize=(28, 20))
-        plt.plot(range(len(y)), y, label="Test Actual", linewidth=3)
+        out = np.array(lit_model2(X))
 
-        [
-            plt.plot(
-                range(len(out)),
-                np.insert(out[:-1], 0, 0),
-                label=f"Test Prediction {i}",
-                linewidth=3,
-            )
-            for i, out in enumerate(outs[:1])
-        ]
-        plt.title("Modified QLSTM", fontsize=24, pad=15)
-        plt.xlabel("Time (hour)", fontsize=20)
-        plt.ylabel("Power (kWh)", fontsize=20)
-        plt.tick_params(axis="both", labelsize=16)
-        plt.legend(fontsize=20, loc="upper right")
+        out = dataset.encoder["label"].inverse_transform(out)
 
-        # Adjust layout to prevent overlap
-        plt.tight_layout()
+        plt.plot(range(len(out)), out, label="M-LSTM", linewidth=3)
 
-        # Show the plot
-        plt.show()
+        out = np.array(lit_model3(X))
 
-        # plt.savefig("./cQLSTMf.png", dpi=300, bbox_inches="tight")
+        out = dataset.encoder["label"].inverse_transform(out)
+
+        plt.plot(range(len(out)), out, label="QLSTM", linewidth=3)
+
+        out = np.array(lit_model4(X))
+
+        out = dataset.encoder["label"].inverse_transform(out)
+
+        plt.plot(range(len(out)), out, label="M-QLSTM", linewidth=3)
+
+    # plt.title("Modified QLSTM", fontsize=24, pad=15)
+    plt.xlabel("Time (hour)", fontsize=20)
+    plt.ylabel("Power (kWh)", fontsize=20)
+    plt.tick_params(axis="both", labelsize=16)
+    plt.legend(fontsize=20, loc="upper right")
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
+
+    # plt.savefig("./cQLSTMf.png", dpi=300, bbox_inches="tight")
 
 
 if __name__ == "__main__":
